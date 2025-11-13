89d68dddfda31c937d16ae64b421bc20d63488e2
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2025-11-12 19:14:53 -0500

    added data files and minor modification to evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 def ann_metrics(r: pd.Series, freq=252):
     mu = r.mean() * freq
@@ -18,4 +19,11 @@ def plot_equity(curves: dict, path_png: str):
         plt.plot(ser.index, ser.values, label=name)
     plt.legend(); plt.title("Cumulative Log Return")
     plt.xlabel("Time"); plt.ylabel("log cum ret")
-    plt.tight_layout(); plt.savefig(path_png, dpi=150); plt.close()
+    plt.tight_layout(); 
+    
+    dir_name = os.path.dirname(path_png)
+    if dir_name:
+        os.makedirs(dir_name, exist_ok=True)
+
+    plt.savefig(path_png, dpi=150)
+    plt.close()
